f5cad08a4f9dc98a7be60cc35bb814e36e53d86a
huydevct <huyvipct1@gmail.com>
2021-10-28 17:14:10 +0700

    init setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>My redux todo app</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Totals todos: 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
